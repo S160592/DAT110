f325365522e1195ce65d21c116a9b4e3c53183d5
s160592 <borgargrande@gmail.com>
2020-02-22 16:31:36 +0100

    endra frå localhost til ip i Controller.java for å teste frå anna maskin

--- dat110-project2/src/no/hvl/dat110/chapp/Controller.java
+++ dat110-project2/src/no/hvl/dat110/chapp/Controller.java
@@ -12,8 +12,8 @@ public class Controller {
 	private Client client;
 	private boolean isconnected = false;
 	
-	private static String DEFAULT_USER = "me";
-	private static String DEFAULT_BROKERHOST = "localhost";
+	private static String DEFAULT_USER = "macbook";
+	private static String DEFAULT_BROKERHOST = "192.168.80.200";
 	private static int DEFAULT_BROKERPORT = 8080;
 	
 	public Controller (MessageArea messagearea) {
